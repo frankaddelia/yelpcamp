397ab96a51b3775a637f35cb79fb76f0c8d30b3d
Frank Addelia <faddelia@gmail.com>
2021-07-02 17:17:29 -0500

    Add ejs-mate and use to create parent layout. Update templates to use the parent layout. Include bootstrap for styles.

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
-const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -18,6 +19,7 @@ db.once('open', () => {
     console.log('Database connected!');
 });
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
