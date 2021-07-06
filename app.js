198b5d6b6b16d2f141d9c91e0f750a602c89bd5e
Frank Addelia <faddelia@gmail.com>
2021-07-06 12:01:44 -0500

    Add Joi for schema valiation (not mongoose validation)

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const catchAsync = require('./utils/catchAsync');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const ExpressError = require('./utils/ExpressError');
+const { campgroundSchema } = require('./schemas.js');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -28,6 +29,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+};
+
 // Routes
 app.get('/', (req, res) => {
     res.render('home');
@@ -38,10 +49,7 @@ app.get('/campgrounds', catchAsync(async (req, res) => {
     res.render('campgrounds/index', { campgrounds });
 }));
 
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
-    if (!req.body.campground) {
-        throw new ExpressError('Invalid Campground Data', 400);
-    }
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -63,7 +71,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     res.render('campgrounds/edit', { campground });
 })); 
 
-app.put('/campgrounds/:id', catchAsync(async(req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async(req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }, { new: true });
     res.redirect(`/campgrounds/${campground._id}`);
