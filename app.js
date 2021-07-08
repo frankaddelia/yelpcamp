b985fd8e1226c511acff0a169dc0b4f6bbaa2567
Frank Addelia <faddelia@gmail.com>
2021-07-08 14:08:41 -0500

    Add post for review form. Add action to review form

--- app.js
+++ app.js
@@ -83,6 +83,10 @@ app.delete('/campgrounds/:id', catchAsync(async(req, res) => {
     res.redirect(`/campgrounds/`);
 }));
 
+app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+    res.send("You made it!");
+}));
+
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page not found', 404));
 });
