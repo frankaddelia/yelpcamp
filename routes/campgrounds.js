1f6d0c777735fb480c94bd3386e5be440d07e8df
Frank Addelia <faddelia@gmail.com>
2021-07-13 17:43:51 -0500

    Populate review authors
    Populate and display author of reviews

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -23,7 +23,17 @@ router.get('/new', isLoggedIn, (req, res) => {
 
 router.get('/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findById(id).populate('reviews').populate('author');
+    const campground = await Campground.findById(id).populate({ 
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        }
+    }).populate('author');
+
+    for (let review of campground.reviews) {
+        console.log(review);
+    }
+
     if (!campground) {
         req.flash('error', 'Campground not found!');
         return res.redirect('/campgrounds');
