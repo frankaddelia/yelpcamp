010fccae004dacfd84983ed188dd9d81e894d176
faddelia <faddelia@gmail.com>
2021-07-13 16:24:10 -0500

    Auth (#3)
    * Add users route and auth for users
    
    * Use passport isAuthenticated helper
    
    Require users to be signed in in order to create a new campground
    
    * Move isAuthenticated into middleware
    
    Remove passport helper isAuthenicated from campgrounds.js into a middleware that can be reused.
    
    * Protect campground routes
    
    Add middleware to ensure users are logged in in order to add/edit/delete campgrounds.
    
    * Auth links
    
    Add links for login/logout/register. Add route to logout.
    
    * Auth links visibility
    
    Add the currentUser to the locals array in app.js and show/hide auth links based on whether the user is logged in or not.
    
    * Fix register route
    
    Log the newly registered user in so they don't have to log in again to be fully logged in
    
    * returnTo url
    
    Add a returnTo property to the session in order to know where the user was originally trying to go when logging in
    
    * Fix login bug
    
    fix login bug where you could still be redirected to /campgrounds/new after logging in even if you weren't logging in from this route.

--- app.js
+++ app.js
@@ -8,9 +8,13 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const methodOverride = require('method-override');
 const ExpressError = require('./utils/ExpressError');
+const passport = require('passport');
+const LocalStratagy = require('passport-local');
+const User = require('./models/user');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -44,17 +48,29 @@ const sessionConfig = {
     }
 };
 app.use(session(sessionConfig));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStratagy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use(flash());
 
 app.use((req, res, next) => {
+    if (!['/login', '/'].includes(req.originalUrl)) {
+        req.session.returnTo = req.originalUrl;
+    }
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 });
 
 // routes middleware
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
+app.use('/', userRoutes);
 
 // Routes
 app.get('/', (req, res) => {
