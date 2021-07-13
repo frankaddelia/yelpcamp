2e3447b1eeadb148f895512e28c1831cbcf66b83
Frank Addelia <faddelia@gmail.com>
2021-07-13 15:44:34 -0500

    Fix login bug
    fix login bug where you could still be redirected to /campgrounds/new after logging in even if you weren't logging in from this route.

--- middleware.js
+++ middleware.js
@@ -1,7 +1,5 @@
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
-        // store the original route to be able to send the user where they originally wanted to go
-        req.session.returnTo = req.originalUrl;
         req.flash('error', 'You must be signed in for that!');
         return res.redirect('/login');
     }
