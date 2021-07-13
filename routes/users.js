979be9c82ab793661033010f65a1a22b4ddf6980
Frank Addelia <faddelia@gmail.com>
2021-07-13 14:48:59 -0500

    Auth links
    Add links for login/logout/register. Add route to logout.

--- routes/users.js
+++ routes/users.js
@@ -30,4 +30,10 @@ router.post('/login', passport.authenticate('local', { failureFlash: true, failu
     res.redirect('/campgrounds');
 });
 
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success', 'Logged out!');
+    res.redirect('/campgrounds');
+});
+
 module.exports = router;
